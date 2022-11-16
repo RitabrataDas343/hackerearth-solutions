c619e659d2572bc1ab9f1927dbc37aca2b42edff
Ritabrata Das <dasritabrata5@gmail.com>
2022-11-16 14:22:56 +0530

    added solutions

---
+++ Basic Programming/Cost of balloons.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+typedef long long int lli;
+typedef long long ll;
+typedef long double ld;
+typedef int_fast64_t fast64;
+typedef uint64_t ut;
+
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+    
+#define ln(x) x.length()
+#define pi 3.1415926535
+#define f(i, a, b) for (lli i = a; i < b; i++)
+#define rf(i, a, b) for (lli i = a; i >= b; i--)
+#define all(x) x.begin(), x.end()
+
+#define Yes cout << "YES" \
+                 << "\n";
+#define No cout << "NO" \
+                << "\n";
+#define auto_init(v, x) \
+    for (auto &x : v)   \
+        cin >> x;
+#define auto_display(v, x) \
+    for (auto x : v)       \
+        cout << x << " ";  \
+    cout << endl;
+#define auto_displayln(v, x) \
+    for (auto x : v)         \
+        cout << x << endl;
+    
+#define vi vector<int>
+#define vc vector<char>
+#define vbool vector<bool>
+#define vll vector<ll>
+#define vst vector<string> 
+#define pb(x) push_back(x);
+
+#define mpi map<int, int>
+#define mpci map<char, int>
+#define mpll map<ll, ll>
+#define pii pair<int, int>
+#define inf INT64_MAX
+
+const lli mod = 1e18;
+    
+using namespace std;
+    
+int main(){
+    IO
+    lli t, g, p, q, a, b, suma, sumb;
+    cin >> t;
+    while(t--){
+        cin >> g >> p;
+        cin >> q;
+        suma = 0, sumb = 0;
+        while(q--){
+            cin >> a >> b;
+            suma += a;
+            sumb += b;
+        }
+        cout << min(suma*g+sumb*p, suma*p+sumb*g) << endl;
+    }
+    return 0;
+}
